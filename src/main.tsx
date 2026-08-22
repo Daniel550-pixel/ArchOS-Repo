b7e88bc77f11e803848f6c7a959ce8ef3dbe0c57
Daniel550-pixel <513785@vistacollege.nl>
2026-08-22 13:37:28 +0200

    feat: mount live runtime pulse in ArchOS shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { RuntimePulseOverlay } from './components/runtime/RuntimePulseOverlay';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <RuntimePulseOverlay />
   </StrictMode>,
 );
