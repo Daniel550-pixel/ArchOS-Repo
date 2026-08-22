b12972a0e9e7de6cf9eb78bfe165e4c8d0e3345a
Daniel550-pixel <513785@vistacollege.nl>
2026-08-22 22:40:06 +0200

    feat(hand): export complete hand interaction stack

--- src/lib/interaction/index.ts
+++ src/lib/interaction/index.ts
@@ -2,3 +2,5 @@ export * from './HandControl';
 export * from './GestureRecognizer';
 export * from './SpatialCommandBus';
 export * from './SpatialInteractionRuntime';
+export * from './MediaPipeHandSource';
+export * from './useSpatialHandControl';
